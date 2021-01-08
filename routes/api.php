dfd2a98632ad919cc39a5424c3d66ceffa4079e3
Gabriel Resende <gabriel.almeida.resende@gmail.com>
2021-01-08 02:31:27 -0300

    otimizacao publiacao

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,14 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 //rotas usuario não autenticado
 Route::post('user', 'App\Http\Controllers\User@insert');
+Route::get('config-cache', function() {
+    $exitCode = Artisan::call('config:cache');
+    // return what you want
+});
 
 Route::group(['middleware' => ['api']], function() {
     Route::post('session', 'App\Http\Controllers\Session@login');
